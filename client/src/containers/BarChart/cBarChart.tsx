3b6ec916c379e49a78f4616562baafa5eddc03f9
thquyen11 <thquyen11@hotmail.com>
2018-12-26 02:35:41 -0500

    finish barchart

--- client/src/containers/BarChart/cBarChart.tsx
+++ client/src/containers/BarChart/cBarChart.tsx
@@ -1,91 +1,102 @@
 import * as React from "react";
 import * as d3 from "d3";
-import "./BarChart.scss";
+import "./cBarChart.scss";
 
 
 
-export class BarChart extends React.Component {
-    constructor(props: any) {
+
+export class BarChart extends React.Component{
+    constructor(props: any){
         super(props);
     }
 
-    drawChar = () => {
-        const dataAPI = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-        fetch(dataAPI)
+    private drawChart = () =>{
+        const dataApi = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+        fetch(dataApi)
             .then(resp => resp.json())
-            .then(result => {
-                const dataset = result.data;
-                const w = 1000;
-                const h = 500;
-                const padding=50;
-                const rect_w = (w-2*padding)/dataset.length;
+            .then(object => object.data)
+            .then(data => {
+                const width:number = 800;
+                const height:number =400;
+                const padding:number=50;
+                const listGDP:any = data.map((data:any, index:number) => data[1]);
+                const listYear:any = data.map((data:any, index:number)=> new Date(data[0]));
 
-                //Scale X Y
-                const arrayYear = dataset.map((data:any[],index:number)=> {
-                    const date=new Date(data[0]);
-                    return date.getTime();
-                }).sort((a:number,b:number)=>(a-b));
-                const minYear = arrayYear[0];
-                const maxYear = arrayYear[arrayYear.length-1];
-                const xScale = d3.scaleTime().domain([minYear, maxYear]).range([padding, w-padding]);
+                const yScale = d3.scaleLinear().domain([0, Math.max(...listGDP)]).range([height-padding, padding]);
+                const xScale = d3.scaleTime().domain([Math.min(...listYear), Math.max(...listYear)]).range([padding, width-padding]);
 
-                const arrayGDP = dataset.map((data:any[],index:number)=> data[1]).sort((a:number, b:number)=>(a-b));
-                const maxGDP = arrayGDP[arrayGDP.length-1];
-                const yScale = d3.scaleLinear().domain([0, maxGDP]).range([h-padding, padding]);
+                const svg = d3.select("#visHolder").append("svg").attr("width", width).attr("height", height);
 
-                //tooltip
-                const tooltip:any = document.createElement("div");
-                        tooltip.setAttribute("id", "tooltip");
-                        tooltip.setAttribute("class", "container");
-
-                //Draw rect
-                const svg = d3.select("#page-wrapper-barchart").append("svg").attr("width", w+"px").attr("height", h+"px").attr("class", "container mx-auto");
-                svg.selectAll("rect").data(dataset).enter()
+                const tooltip = d3.select("#visHolder").append("div")
+                    .attr("class", "container")
+                    .attr("id", "tooltip")
+                    .style("visibility", "hidden")
+                
+                const barWidth:number = (width-2*padding)/data.length;
+                svg.selectAll("rect").data(data).enter()
                     .append("rect")
-                        .attr("x", (d:any)=> {
-                            const date = new Date(d[0]);
+                        .attr("x", d => {
+                            const date:Date = new Date(d[0]);
                             return xScale(date.getTime());
-                        }).attr("y", (d:any)=> {
-                            return yScale(d[1])
-                        }).attr("width", rect_w+"px")
-                        .attr("height", (d:any) => h-padding - yScale(d[1]))
+                        })
+                        .attr("y", d=> yScale(d[1]))
+                        .attr("width", barWidth)
+                        .attr("height", d=> (height-padding)-yScale(d[1]))
                         .attr("class", "bar")
-                    .on("mouseover", (d:any, i:number)=>{
-                        const yearmonth= d[0].slice(0,7);
-                        const tooltipContent:string= `<div class="row justify-content-center">` + yearmonth +`</div><div class="row justify-content-center">$`+d[1]+ " Billions</div>";
+                        .attr("data-date", d=> d[0])
+                        .attr("data-gdp", d=> d[1])
+                        .on("mouseover", (d, i)=>{
+                            tooltip
+                                .attr("data-date", d[0])
+                                .transition().duration(50)
+                                .style("visibility", "visible")
+                                .style('left', i * barWidth + 200 + 'px')
+                                .style('top', 5/8*height +"px")
+                            tooltip
+                                .html(() => {
+                                    const year = d[0].slice(0,7);
+                                    const tooltipContent:string= `<div class="row justify-content-center">` + year +`</div><div class="row justify-content-center">$`+d[1]+ " Billions</div>";
+                                    return tooltipContent;
+                                })
+                        })
+                        .on("mouseout", ()=>{
+                            tooltip
+                                .transition().duration(100)
+                                .style("visibility", "hidden");
+                        })
 
-                        tooltip.setAttribute("style", "position: absolute; visibility: visible; top: 350px; left: "+(i*rect_w+padding+70)+"px");
-                        tooltip.innerHTML= tooltipContent;
-                        
-                        document.querySelector("#page-wrapper-barchart")!.appendChild(tooltip);
-                    })
-                    .on("mouseout", (d:any, i:number)=>{
-                        tooltip.setAttribute("style", "visibility: hidden");
-                    })
+                //Axies, titles
+                svg.append('text').
+                    attr('transform', 'rotate(-90)').
+                    attr('x', -200).
+                    attr('y', 80).
+                    text('Gross Domestic Product');
                 
-                //Title, axises
-                svg.append("text").attr("x", w/3).attr("y", 50).attr("style", "font-size: 36px").text("UNITED STATES GDP");
-                svg.append("text").attr("transform", "rotate(90deg)").attr("x", padding+10).attr("y", padding + 150).text("Gross Dosmetic Product");
+                const xAxis:any = d3.axisBottom(xScale);
+                const yAxis:any = d3.axisLeft(yScale);
+                svg.append("g").call(xAxis)
+                    .attr("id", "x-axis")
+                    .attr("transform", "translate(0 " + (height-padding) +")");
+                svg.append("g").call(yAxis)
+                    .attr("id", "y-axis")
+                    .attr("transform", "translate("+padding+" 0)");
 
-                const xAxis: any = d3.axisBottom(xScale);
-                const yAxis: any = d3.axisLeft(yScale);
-                svg.append("g").call(xAxis).attr("id", "x-axis").attr("transform", "translate(0 "+(h-padding)+")");
-                svg.append("g").call(yAxis).attr("id", "y-axis").attr('transform', "translate("+padding+", 0)");
-                
-                
             })
-            .catch(err => console.log(err))
     }
 
-    componentDidMount() {
-        //only load chart after components mounted to speed up page loading time
-        this.drawChar();
+    componentDidMount(){
+        this.drawChart();
     }
 
-    render() {
-        return (
-            <div className="container mx-auto" id="page-wrapper-barchart">
+    render(){
+        return(
+            <div className="container" id="pagewrapper-barchart">
+                <div id="title" className="text-center">
+                    <h1>United States GDP</h1>
+                </div>
+                <div id="visHolder" className="row"></div>
             </div>
         )
     }
+    
 }
