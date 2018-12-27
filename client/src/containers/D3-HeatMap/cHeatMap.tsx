145fd8e022e7ff0698da2b9cc595ac3dafe57f5d
thquyen11 <thquyen11@hotmail.com>
2018-12-27 22:41:40 +0700

    finish heatmap

--- client/src/containers/D3-HeatMap/cHeatMap.tsx
+++ client/src/containers/D3-HeatMap/cHeatMap.tsx
@@ -40,12 +40,7 @@ export class HeatMap extends React.Component{
                     .domain([Math.min(...monthlyVariance.map((d:any)=> d.year)), Math.max(...monthlyVariance.map((d:any)=>d.year))])
                     .range([0, width]);
                 const xAxis:any = d3.axisBottom(xScale)
-                    .tickValues(xScale.domain().filter((year:number)=> year%10===0))
-                    .tickFormat((year:number)=> {
-                        let date = new Date(0);
-                        date.setUTCFullYear(year);
-                        return date.getUTCFullYear.toString();
-                    });
+                    .tickFormat(d3.format("d"))
 
                 const months:string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
                 const yScale = d3.scaleBand()
@@ -56,10 +51,10 @@ export class HeatMap extends React.Component{
                 const barWidth:number = width/(monthlyVariance.length/12);
                 const barHeight:number=height/12;
 
-                const colorScheme:string[] = ["rgb(49, 54, 149)", "rgb(69, 117, 180)", "rgb(116, 173, 209)", "rgb(171, 217, 233)", "rgb(224, 243, 248)", "rgb(255, 255, 191)", "rgb(254, 224, 144)", "rgb(253, 174, 97)", "rgb(244, 109, 67)", "rgb(215, 48, 39)", "rgb(165, 0, 38)"];
+                const colorbrew:string[]=['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'];
                 const colorScale:any = d3.scaleQuantize<string>()
                     .domain([minTemp, maxTemp])
-                    .range(colorScheme);
+                    .range(colorbrew.reverse());
 
                 const tooltip:any = d3.select("body").append("div")
                     .attr("id", "tooltip")
@@ -71,7 +66,7 @@ export class HeatMap extends React.Component{
                 groupChart.selectAll("rect").data(monthlyVariance).enter()
                     .append("rect")
                         .attr("class", "cell")
-                        .attr("data-month", (d:any)=> d.month)
+                        .attr("data-month", (d:any)=> d.month-1)
                         .attr("data-year", (d:any)=> d.year)
                         .attr("data-temp", (d:any)=> d.variance)
                         .attr("x", (d:any)=> xScale(d.year))
@@ -86,7 +81,7 @@ export class HeatMap extends React.Component{
                                 .attr("data-year", d.year)
                                 .style("left", (d3.event.pageX)+"px")
                                 .style("top", (d3.event.pageY-barHeight*3)+"px")
-                                .html("Year "+d.year+" - Month "+d.month+"</br>"+ Math.round((data.baseTemperature+d.variance)*100)/100 +"oC"+"</br>"+ Math.round((data.baseTemperature-d.variance)*100)/100+"oC")
+                                .html("Year "+d.year+" - Month "+d.month+"</br>"+ Math.round((d.variance+data.baseTemperature)*100)/100 +"oC"+"</br>"+ Math.round((d.variance)*100)/100+"oC")
                                 .style("visibility", "visible")
                         })
                         .on("mouseout", ()=>{
@@ -120,38 +115,40 @@ export class HeatMap extends React.Component{
                     .attr("transform", "translate("+ margin.left+" "+(margin.top+height+margin.bottom/2)+")")
 
                 const legendWidth:number = 300;
-                const colorbrew:string[]=['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'];
+                
                 const legendRectWidth:number = legendWidth/colorbrew.length;
 
                 const array:number[]=[];
                 for(let i=minTemp; i<=maxTemp; i+= (maxTemp-minTemp)/(colorbrew.length)){
-                    array.push(i);
+                    if(i>minTemp){
+                        array.push(i);
+                    }
                 }
 
-                const xScaleLegend:any = d3.scaleLinear()
-                    .domain([minTemp, maxTemp])
-                    .range([0, legendWidth])
+                const arrayX:number[]=[];
+                for(let i:number=0; i<=legendWidth; i+=legendWidth/colorbrew.length){
+                    arrayX.push(i)
+                }
+
+                const xScaleLegend:any = d3.scaleThreshold()
+                    .domain(array)
+                    .range(arrayX)
                     
-                const xAxisLegend:any = d3.axisBottom(xScaleLegend).tickValues(array).tickFormat(d3.format(".1f"));
+                const xAxisLegend:any = d3.axisBottom(xScaleLegend).ticks(10).tickFormat(d3.format(".1f"));
 
                 groupLegend.append("g")
-                    .attr("transform", "translate("+legendRectWidth+" "+legendRectWidth+")")
+                    .attr("transform", "translate(0 "+legendRectWidth+")")
                     .call(xAxisLegend)
                 
-                const scaleLegendColor:any = d3.scaleThreshold<number,string>()
-                    .domain(array)
-                    .range(colorbrew.reverse())
-
-                groupLegend.selectAll("rect").data(array).enter()
+                groupLegend.selectAll("rect").data(colorbrew).enter()
                     .append("rect")
-                        .attr("class", "bar")
+                        .attr("class", "cell")
                         .attr("x", (color:any,index:number)=> index*legendRectWidth)
                         .attr("y", 0)
                         .attr("color", (d:any)=> d)
                         .style("width", legendRectWidth)
                         .style("height", legendRectWidth)
-                        .style("fill", (color:any)=> scaleLegendColor(color))
-
+                        .style("fill", (color:any)=> color)
             })
         
     }
