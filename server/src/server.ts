128232ac044db9cdc63cdd8502d4ff3dbacebeb3
thquyen11 <thquyen11@hotmail.com>
2019-01-02 07:54:28 -0500

    add Tracker project

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@
 import * as express from "express";
 import { Request, Response } from "express";
 import * as bodyParser from "body-parser";
+import * as knex from "knex";
 require('dotenv').config();
 require('ts-node').register();
 
@@ -14,9 +15,19 @@ app.listen(process.env.PORT, ()=>{
     console.log("Server running on port ", process.env.PORT);
 })
 
-const db:any={
+const dbTemp:any={
     shortenURL:[],
 }
+
+const db:any = knex({
+    client: 'pg',
+    connection:{
+        host: process.env.DB_HOST,
+        database: process.env.DB,
+        user: process.env.DB_USER,
+        password: process.env.DB_USERPASS,
+    }
+})
 
 app.get("/", (req:Request, res:Response)=>{
     console.log(req);
@@ -67,10 +78,10 @@ app.post("/api/shorturl/new", (req:Request, res:Response)=>{
     const url:string = req.body.replace(/\r\n|\r|\n/gm, "");
     const regex:any = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
     if(url.match(regex)){
-        db.shortenURL.push(url);
+        dbTemp.shortenURL.push(url);
         res.status(200).json({
             "original_url": url,
-            "short_url": (db.shortenURL.length-1),
+            "short_url": (dbTemp.shortenURL.length-1),
         })
     } else{
         res.status(400).json(()=>{
@@ -85,18 +96,25 @@ app.get("/api/shorturl/:shortURL", (req:Request, res:Response)=>{
     const { shortURL } = req.params;
     const shortUrl:number = parseInt(shortURL,10);
 
-    if(shortUrl>(db.shortenURL.length-1)){
+    if(shortUrl>(dbTemp.shortenURL.length-1)){
         res.status(400).json(()=>{
             return{
                 "error": "invalid URL",
             }
         })
     } else{
-        const originalUrl:string = db.shortenURL[shortUrl];
+        const originalUrl:string = dbTemp.shortenURL[shortUrl];
         res.redirect(originalUrl);
     }
 })
 
+
+// FCC TRACKER project
+app.post("/api/exercise/new-user", (req:Request, res:Response)=>{
+    const userName:string = req.body;
+
+    
+})
 
 
 
